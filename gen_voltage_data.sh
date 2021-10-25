7257ced2f12137a7936e6f3447131d2c40df55df
shuji-oh <ohira.shuji.ok2@is.naist.jp>
2021-10-25 15:52:14 +0900

    add shell script for generation of csv data

--- gen_voltage_data.sh
+++ gen_voltage_data.sh
@@ -10,6 +10,6 @@ ls -w 1 20211022-ard2/ | xargs -I '{}' python3 extract_all.py '20211022-ard2/{}'
 
 ls -w 1 20211022-suzuki_ecu/ | xargs -I '{}' python3 extract_all.py '20211022-suzuki_ecu/{}' ECU4 >> voltage.csv  
 
-ls -w 1 20211025-rpi_yellow/ | xargs -I '{}' python3 extract_all.py '20211023-rpi_yellow/{}' ECU5 >> voltage.csv
+ls -w 1 20211025-rpi_yellow/ | xargs -I '{}' python3 extract_all.py '20211025-rpi_yellow/{}' ECU5 >> voltage.csv
 
 ls -w 1 20211022-suzuki_meter/ | xargs -I '{}' python3 extract_all.py '20211022-suzuki_meter/{}' ECU6 >> voltage.csv
